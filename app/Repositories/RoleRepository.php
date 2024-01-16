0a86da06c6f63354601cbf1686a146c823e2a61c
tarequr <tarequrr8@gmail.com>
2024-01-16 23:03:14 +0600

    processing

--- app/Repositories/RoleRepository.php
+++ app/Repositories/RoleRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repositories;
 
 use App\Models\Role;
+use App\Models\Permission;
+use Illuminate\Support\Facades\Log;
 
 class RoleRepository implements RoleInterface
 {
@@ -25,10 +27,59 @@ class RoleRepository implements RoleInterface
 
     public function create(array $data, $permissions)
     {
-        return $this->role->create($data)->permissions()->sync(
-                    $permissions,
-                    ['name' => 'demo name']
-                );
+
+        $modifiedPermissions = [];
+
+        // $name = [
+        //     1 => ['name' => 'tarequr'],
+        //     2 => ['name' => 'tarequr 2'],
+        //     3 => ['name' => 'tarequr 3'],
+        //     4 => ['name' => 'tarequr 4']
+        // ];
+
+        foreach ($permissions as $key => $permission) {
+            $permissionName = Permission::find($permission)->pluck('name');
+
+            // Log::info($permission);
+            // Log::info([$permission => ['name' => $permissionName || ""]]);
+            // return [$permission => ['name' => $permissionName || ""]];
+            array_push($modifiedPermissions, [$permission => ['name' => $permissionName]]);
+        }
+
+
+
+
+        $outputArray = [];
+
+        foreach ($modifiedPermissions as $subArray) {
+            foreach ($subArray as $key => $value) {
+                Log::info($value);
+return "";
+                $outputArray[$key] = $value;
+            }
+        }
+
+
+
+        // array_map($modifiedPermissions, function ($permission){
+        //     return [ => $permission];
+        // });
+
+        // Log::info($outputArray);
+        // Log::info($modifiedPermissions);
+        // var_dump($modifiedPermissions);
+        // return $modifiedPermissions;
+
+        return $this->role->create($data)->permissions()->attach(
+            $outputArray
+            // $permissions,
+            // ['name' => function($permission){
+            //     $per = Permission::find($permission)->select('name');
+
+            //     Log::info($per->name);
+            //     return $per->name;
+            // }]
+        );
     }
 
     public function update(array $data, $id)
